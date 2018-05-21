949301fff2a5f8acb9f6f5357151f24254a109a8
JohnnyJayJay <39348311+JohnnyJayJay@users.noreply.github.com>
2018-05-21 15:34:48 +0200

    Update CommandListener.java

--- src/main/java/de/johnnyjayjay/discord/api/command/CommandListener.java
+++ src/main/java/de/johnnyjayjay/discord/api/command/CommandListener.java
@@ -10,7 +10,7 @@ class CommandListener extends ListenerAdapter {
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 
-        if (event.getMessage().getContentRaw().startsWith(getPrefix())) {
+        if (event.getMessage().getContentRaw().startsWith(getPrefix()) && !event.getAuthor().isBot()) {
             CommandHandler.handleCommand(event.getMessage().getContentRaw(), event);
         }
 
