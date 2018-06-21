abe0c0c8d95de4a7f41926f7b420804bb403ffae
JohnnyJayJay <39348311+JohnnyJayJay@users.noreply.github.com>
2018-06-21 13:25:58 +0200

    Update CommandListener.java
    Quick commit, cleaned the code of CommandListener a bit.

--- src/main/java/de/johnnyjayjay/discord/api/command/CommandListener.java
+++ src/main/java/de/johnnyjayjay/discord/api/command/CommandListener.java
@@ -25,28 +25,25 @@ class CommandListener extends ListenerAdapter {
                 cmd.getExecutor().onCommand(new CommandEvent(event.getJDA(), event.getResponseNumber(), event.getMessage(), cmd),
                         event.getMember(), event.getChannel(), cmd.getArgs());
             } else if (settings.useHelpCommand() && settings.getHelpLabels().contains(cmd.getLabel())) {
-                if (cmd.getArgs().length == 0) {
-                    this.sendInfo(event.getChannel(), null, null);
-                } else if (cmd.getArgs().length == 1 && settings.getCommands().containsKey(cmd.getArgs()[0])) {
-                    this.sendInfo(event.getChannel(), settings.getCommands().get(cmd.getArgs()[0]), cmd.getArgs()[0]);
-                }
+                this.sendInfo(event.getChannel(), cmd.getArgs());
             }
         }
     }
 
-    private void sendInfo(TextChannel channel, ICommand command, String label) {
+    private void sendInfo(TextChannel channel, String[] args) {
         var builder = new EmbedBuilder().setTitle("Help");
-        if (command == null) {
+        if (args.length == 0) {
             String helpLabels = format("[%s]", String.join("|", settings.getHelpLabels().toArray(new String[settings.getHelpLabels().size()])));
             builder.appendDescription(format("To learn more about a specific command, just call `%s%s <label>`.\n", settings.getPrefix(), helpLabels))
                     .appendDescription("The following commands are currently available:\n");
             var commandLabels = settings.getCommands().keySet().toArray(new String[settings.getCommands().keySet().size()]);
             builder.addField("Commands", format("```\n%s%s```", settings.getPrefix(), String.join(format("\n%s", settings.getPrefix()), commandLabels)), false);
             channel.sendMessage(builder.build()).queue();
-        } else {
-            builder.appendDescription(format("Command Info for: `%s`\n\n", label))
-                    .appendDescription(command.info());
+        } else if (args.length == 1 && settings.getCommands().containsKey(args[0])) {
+            builder.appendDescription(format("Command Info for: `%s`\n\n", args[0]))
+                    .appendDescription(settings.getCommands().get(args[0]).info());
             channel.sendMessage(builder.build()).queue();
         }
     }
 }
+
