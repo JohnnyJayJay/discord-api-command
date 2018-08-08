f529a75bce3c8c041e9d01f66f33c10b5013c338
Unknown <johnnyjayjay02@gmail.com>
2018-08-08 22:09:27 +0200

    last import optimization

--- src/main/java/com/github/johnnyjayjay/discord/commandapi/SubCommand.java
+++ src/main/java/com/github/johnnyjayjay/discord/commandapi/SubCommand.java
@@ -7,7 +7,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.util.function.Consumer;
 
 /**
  * This annotation may be used inside of a sub class of AbstractCommand to annotate methods as SubCommand-methods.
